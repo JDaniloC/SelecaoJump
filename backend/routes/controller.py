6a302eb85be8ffca8e969cfcd61eb195cd69a0b0
André Luiz <alps2@cin.ufpe.br>
2023-05-19 12:11:05 -0300

    dashboard stats

--- backend/routes/controller.py
+++ backend/routes/controller.py
@@ -8,7 +8,7 @@ class Core:
     def load_log_data(self, file_path):
         """ Loads the csv in file_path into an event_log. """
         columns_to_datetime = ['Start', 'End']
-        df = pandas.read_csv(file_path, encoding = "utf-8", sep=';', parse_dates=columns_to_datetime, on_bad_lines="skip")
+        df = pandas.read_csv(file_path, encoding = "utf-8", sep=';', parse_dates=columns_to_datetime)
         df = pm4py.format_dataframe(df, case_id="Case", activity_key="ActivityCode", timestamp_key="End", start_timestamp_key="Start")
         self.log = pm4py.convert_to_event_log(df)
 
--- backend/routes/dashboard.py
+++ backend/routes/dashboard.py
@@ -9,10 +9,51 @@ router = APIRouter(
     responses={404: {"dashboard": "Not found"}}
 )
 
-@router.get("/test/", status_code=200)
-async def get_dashboard_infos():
-    """ Test route. """
+@router.get("/stats/", status_code=200)
+async def get_log_stats():
+    """
+    Returns statistics about the log:
+    {
+        "casesCount": Total de casos no log,
+        "activitiesCount": Total de atividades no log,
+        "avgActivitiesPerCase": Média de atividades por caso,
+        "avgCaseDuration": Duração média, em segundos, de cada caso,
+        "avgActivityDuration: Duração média, em segundos, de cada atividade
+    }
+    """
+    event_log = core_instance.log
+
+    activity_count = []
+    case_durations = []
+    activity_durations = []
+
+    for trace in event_log:
+        trace_duration = 0
+        activity_count.append(len(trace))
+
+        for activity in trace:
+            activity_duration = (activity['End'] - activity['Start']).total_seconds()
+            activity_durations.append(activity_duration)
+            trace_duration += activity_duration
+
+        case_durations.append(trace_duration)
+
+    avg_case_duration = 0
+    if len(case_durations) > 0:
+        avg_case_duration = round(sum(case_durations) / len(case_durations), 2)
+    
+    avg_atividades_duration = 0
+    if len(activity_durations) > 0:
+        avg_atividades_duration = round(sum(activity_durations) / len(activity_durations), 2)
+    
+    avg_atividades_count_per_case = 0
+    if len(activity_count) > 0:
+        avg_atividades_count_per_case = round(sum(activity_count) / len(activity_count), 2)
 
     return {
-        "test": len(core_instance.log)
+        "casesCount": len(event_log),
+        "activitiesCount": sum(activity_count),
+        "avgActivitiesPerCase": avg_atividades_count_per_case,
+        "avgCaseDuration": avg_case_duration,
+        "avgActivityDuration": avg_atividades_duration
     }

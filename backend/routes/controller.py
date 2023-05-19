507374da481cece4fadce6c642742632d9b46f7c
André Luiz <alps2@cin.ufpe.br>
2023-05-19 12:36:50 -0300

    get processos infos

--- backend/routes/controller.py
+++ backend/routes/controller.py
@@ -8,7 +8,8 @@ class Core:
     def load_log_data(self, file_path):
         """ Loads the csv in file_path into an event_log. """
         columns_to_datetime = ['Start', 'End']
-        df = pandas.read_csv(file_path, encoding = "utf-8", sep=';', parse_dates=columns_to_datetime)
+        dtypes = {"Case": str}
+        df = pandas.read_csv(file_path, encoding = "utf-8", sep=';', parse_dates=columns_to_datetime, dtype=dtypes)
         df = pm4py.format_dataframe(df, case_id="Case", activity_key="ActivityCode", timestamp_key="End", start_timestamp_key="Start")
         self.log = pm4py.convert_to_event_log(df)
 
--- backend/routes/dashboard.py
+++ backend/routes/dashboard.py
@@ -9,6 +9,11 @@ router = APIRouter(
     responses={404: {"dashboard": "Not found"}}
 )
 
+def get_activity_duration(activity: dict):
+    """ Returns the duration of an activity, in seconds. """
+    return (activity['End'] - activity['Start']).total_seconds()
+
+
 @router.get("/stats/", status_code=200)
 async def get_log_stats():
     """
@@ -32,7 +37,7 @@ async def get_log_stats():
         activity_count.append(len(trace))
 
         for activity in trace:
-            activity_duration = (activity['End'] - activity['Start']).total_seconds()
+            activity_duration = get_activity_duration(activity)
             activity_durations.append(activity_duration)
             trace_duration += activity_duration
 
@@ -57,3 +62,26 @@ async def get_log_stats():
         "avgCaseDuration": avg_case_duration,
         "avgActivityDuration": avg_atividades_duration
     }
+
+@router.get("/processos/{pinned_activity}/", status_code=200)
+async def get_processos_infos(pinned_activity: str):
+    """ Returns a list of all processos with some stats and a count of how many times the given activity happened. """
+    event_log = core_instance.log
+    cases = {}
+
+    for trace in event_log:
+        case = trace.attributes['concept:name']
+        pinned_activity_count = 0
+        duration = 0
+
+        for activity in trace:
+            if activity['concept:name'] == pinned_activity:
+                pinned_activity_count += 1
+            duration += get_activity_duration(activity)
+
+        cases[case] = {
+            "duration": duration,
+            "pinnedActivityCount": pinned_activity_count,
+        }
+
+    return {"cases": cases}
